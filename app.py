552292f690b758badd96ad71c61457efa6d92ed7
panchanyam <panchanyam.j@gmail.com>
2026-04-17 01:28:36 +0530

    Update app for Render deployment

--- app.py
+++ app.py
@@ -140,5 +140,8 @@ threading.Thread(target=monitor, daemon=True).start()
 # ===============================
 # RUN APP
 # ===============================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
